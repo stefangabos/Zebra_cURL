5395a71cabee01315cd166b27b828f4a6004cebb
stefangabos <contact@stefangabos.ro>
2013-08-29 15:58:48 +0300

    Updated version number to 1.0.2

--- Zebra_cURL.php
+++ Zebra_cURL.php
@@ -28,7 +28,7 @@
  *  For more resources visit {@link http://stefangabos.ro/}
  *
  *  @author     Stefan Gabos <contact@stefangabos.ro>
- *  @version    1.0.1 (last revision: May 30, 2013)
+ *  @version    1.0.2 (last revision: August 29, 2013)
  *  @copyright  (c) 2013 Stefan Gabos
  *  @license    http://www.gnu.org/licenses/lgpl-3.0.txt GNU LESSER GENERAL PUBLIC LICENSE
  *  @package    Zebra_cURL
