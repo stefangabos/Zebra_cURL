5e8ad48e637a0701b882ec126f4b7ab7423dcb80
Stefan Gabos <contact@stefangabos.ro>
2015-01-17 00:32:26 +0200

    Updated code

--- examples/rss-feeds.php
+++ examples/rss-feeds.php
@@ -2,11 +2,25 @@
 
 function callback($result) {
 
-    // remember, the "body" property of $result is run through "htmlentities()", so you may need to "html_entity_decode" it
+    // everything went well at cURL level
+    if ($result->response[1] == CURLE_OK) {
 
-    // show everything
-    print_r('<pre>');
-    print_r($result->info);
+        // if server responded with code 200 (meaning that everything went well)
+        // see http://httpstatus.es/ for a list of possible response codes
+        if ($result->info['http_code'] == 200) {
+
+            // see all the returned data
+            // remember, that the "body" property of $result, unless specifically disabled in the library's constructor,
+            // is run through "htmlentities()", so you may want to "html_entity_decode" it
+            print_r('<pre>');
+            print_r($result->info);
+
+        // show the server's response code
+        } else die('Server responded with code ' . $result->info['http_code']);
+
+    // something went wrong
+    // ($result still contains all data that could be gathered)
+    } else die('cURL responded with: ' . $result->response[0]);
 
 }
 
@@ -24,7 +38,6 @@ $curl->get(array(
     'http://rss1.smashingmagazine.com/feed/',
     'http://allthingsd.com/feed/',
     'http://feeds.feedburner.com/nettuts',
-    'http://www.webmonkey.com/feed/',
     'http://feeds.feedburner.com/alistapart/main',
 ), 'callback');
 
