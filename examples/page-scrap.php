927506cc2a0991a7769e6e27f6469b4c2de89850
Stefan Gabos <contact@stefangabos.ro>
2019-01-28 14:13:30 +0200

    Fixed example

--- examples/page-scrap.php
+++ examples/page-scrap.php
@@ -9,8 +9,12 @@ $curl = new Zebra_cURL();
 // cache results 3600 seconds
 $curl->cache('cache', 3600);
 
+// since we are also comunicating over HTTPS, we load a CA bundles so we don't get
+// CURLE_SSL_CACERT response from cURL
+$curl->ssl(true, 2, __DIR__ . '/cacert.pem');
+
 // a simple way of scrapping a page
 // (you can do more with the "get" method and callback functions)
-echo $curl->scrap('https://google.com', true);
+echo $curl->scrap('https://github.com/', true);
 
 ?>
