29f76674f84946048d8ba0cb8345988ce878c166
Stefan Gabos <contact@stefangabos.ro>
2019-01-28 14:21:17 +0200

    Fixed example

--- examples/ftp-download.php
+++ examples/ftp-download.php
@@ -6,8 +6,11 @@ require '../Zebra_cURL.php';
 // instantiate the Zebra_cURL class
 $curl = new Zebra_cURL();
 
+// since we are comnunicating over HTTPS, we load a CA bundles so we don't get CURLE_SSL_CACERT response from cURL
+$curl->ssl(true, 2, __DIR__ . '/cacert.pem');
+
 // get a random file from mozilla's public ftp server at http://ftp.mozilla.org/
-$curl->ftp_download('http://ftp.mozilla.org/pub/mozilla.org/webtools/bugzilla-4.0-to-4.0.5-nodocs.diff.gz', 'cache');
+$curl->ftp_download('https://ftp.mozilla.org/pub/firefox/releases/65.0/KEY', 'cache');
 
 echo 'File downloaded - look in the "cache" folder!';
 
