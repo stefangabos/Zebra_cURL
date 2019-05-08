042d615888ba054d443015ebebae12d39c6fdc40
stefangabos <contact@stefangabos.ro>
2019-05-08 11:38:09 +0300

    Bumped revision date

--- Zebra_cURL.php
+++ Zebra_cURL.php
@@ -6,7 +6,7 @@
  *  Read more {@link https://github.com/stefangabos/Zebra_cURL/ here}
  *
  *  @author     Stefan Gabos <contact@stefangabos.ro>
- *  @version    1.4.0 (last revision: January 30, 2019)
+ *  @version    1.4.0 (last revision: May 08, 2019)
  *  @copyright  (c) 2013 - 2019 Stefan Gabos
  *  @license    http://www.gnu.org/licenses/lgpl-3.0.txt GNU LESSER GENERAL PUBLIC LICENSE
  *  @package    Zebra_cURL
