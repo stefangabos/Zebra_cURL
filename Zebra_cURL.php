ce5e0c4bb88cee18be4e72d3e8842db4c2b2b305
stefangabos <contact@stefangabos.ro>
2020-07-20 14:32:49 +0300

    Updated version number

--- Zebra_cURL.php
+++ Zebra_cURL.php
@@ -6,8 +6,8 @@
  *  Read more {@link https://github.com/stefangabos/Zebra_cURL/ here}
  *
  *  @author     Stefan Gabos <contact@stefangabos.ro>
- *  @version    1.4.0 (last revision: May 08, 2019)
- *  @copyright  (c) 2013 - 2019 Stefan Gabos
+ *  @version    1.5.0 (last revision: July 20, 2020)
+ *  @copyright  (c) 2013 - 2020 Stefan Gabos
  *  @license    http://www.gnu.org/licenses/lgpl-3.0.txt GNU LESSER GENERAL PUBLIC LICENSE
  *  @package    Zebra_cURL
  */
