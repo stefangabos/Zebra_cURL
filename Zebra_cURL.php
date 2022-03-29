afa2fe691ad6187c492b99878bf0b4e16c76220c
stefangabos <contact@stefangabos.ro>
2022-03-30 00:24:17 +0300

    Updated list of possible curl response messages; see #44

--- Zebra_cURL.php
+++ Zebra_cURL.php
@@ -6,7 +6,7 @@
  *  Read more {@link https://github.com/stefangabos/Zebra_cURL/ here}.
  *
  *  @author     Stefan Gabos <contact@stefangabos.ro>
- *  @version    1.6.0 (last revision: March 17, 2022)
+ *  @version    1.7.0 (last revision: March 30, 2022)
  *  @copyright  © 2013 - 2022 Stefan Gabos
  *  @license    https://www.gnu.org/licenses/lgpl-3.0.txt GNU LESSER GENERAL PUBLIC LICENSE
  *  @package    Zebra_cURL
@@ -108,6 +108,7 @@ class Zebra_cURL {
 
     /**
      *  Possible values of the "result" attribute in the object passed to the callback function.
+     *  List is kept up to date with the {@link https://github.com/curl/curl/blob/master/include/curl/curl.h source code of curl}
      *
      *  @var array
      *
@@ -118,53 +119,65 @@ class Zebra_cURL {
         1   =>  'CURLE_UNSUPPORTED_PROTOCOL',
         2   =>  'CURLE_FAILED_INIT',
         3   =>  'CURLE_URL_MALFORMAT',
-        4   =>  'CURLE_URL_MALFORMAT_USER',
+        4   =>  'CURLE_NOT_BUILT_IN',
         5   =>  'CURLE_COULDNT_RESOLVE_PROXY',
         6   =>  'CURLE_COULDNT_RESOLVE_HOST',
         7   =>  'CURLE_COULDNT_CONNECT',
-        8   =>  'CURLE_FTP_WEIRD_SERVER_REPLY',
+        8   =>  'CURLE_WEIRD_SERVER_REPLY',
         9   =>  'CURLE_REMOTE_ACCESS_DENIED',
+        10  =>  'CURLE_FTP_ACCEPT_FAILED',
         11  =>  'CURLE_FTP_WEIRD_PASS_REPLY',
+        12  =>  'CURLE_FTP_ACCEPT_TIMEOUT',
         13  =>  'CURLE_FTP_WEIRD_PASV_REPLY',
         14  =>  'CURLE_FTP_WEIRD_227_FORMAT',
         15  =>  'CURLE_FTP_CANT_GET_HOST',
+        16  =>  'CURLE_HTTP2',
         17  =>  'CURLE_FTP_COULDNT_SET_TYPE',
         18  =>  'CURLE_PARTIAL_FILE',
-        19  =>  'CURLE_FTP_COULDNT_RETR_FILE',
+        19  =>  'CURLE_FTP_COULDNT_RETR_FILE',     // NOT USED
+        20  =>  'CURLE_OBSOLETE20',
         21  =>  'CURLE_QUOTE_ERROR',
         22  =>  'CURLE_HTTP_RETURNED_ERROR',
-        23  =>  'CURLE_WRITE_ERROR',
+        23  =>  'CURLE_WRITE_ERROR',               // NOT USED
+        24  =>  'CURLE_OBSOLETE24',
         25  =>  'CURLE_UPLOAD_FAILED',
         26  =>  'CURLE_READ_ERROR',
         27  =>  'CURLE_OUT_OF_MEMORY',
-        28  =>  'CURLE_OPERATION_TIMEDOUT',
+        28  =>  'CURLE_OPERATION_TIMEDOUT',        // NOT USED
+        29  =>  'CURLE_OBSOLETE29',
         30  =>  'CURLE_FTP_PORT_FAILED',
-        31  =>  'CURLE_FTP_COULDNT_USE_REST',
+        31  =>  'CURLE_FTP_COULDNT_USE_REST',      // NOT USED
+        32  =>  'CURLE_OBSOLETE32',
         33  =>  'CURLE_RANGE_ERROR',
         34  =>  'CURLE_HTTP_POST_ERROR',
         35  =>  'CURLE_SSL_CONNECT_ERROR',
         36  =>  'CURLE_BAD_DOWNLOAD_RESUME',
         37  =>  'CURLE_FILE_COULDNT_READ_FILE',
         38  =>  'CURLE_LDAP_CANNOT_BIND',
-        39  =>  'CURLE_LDAP_SEARCH_FAILED',
+        39  =>  'CURLE_LDAP_SEARCH_FAILED',        // NOT USED
+        40  =>  'CURLE_OBSOLETE40',                // NOT USED SINCE curl 7.53.0
         41  =>  'CURLE_FUNCTION_NOT_FOUND',
         42  =>  'CURLE_ABORTED_BY_CALLBACK',
-        43  =>  'CURLE_BAD_FUNCTION_ARGUMENT',
-        45  =>  'CURLE_INTERFACE_FAILED',
+        43  =>  'CURLE_BAD_FUNCTION_ARGUMENT',     // NOT USED
+        44  =>  'CURLE_OBSOLETE44',
+        45  =>  'CURLE_INTERFACE_FAILED',          // NOT USED
+        46  =>  'CURLE_OBSOLETE46',
         47  =>  'CURLE_TOO_MANY_REDIRECTS',
-        48  =>  'CURLE_UNKNOWN_TELNET_OPTION',
-        49  =>  'CURLE_TELNET_OPTION_SYNTAX',
-        51  =>  'CURLE_PEER_FAILED_VERIFICATION',
+        48  =>  'CURLE_UNKNOWN_OPTION',
+        49  =>  'CURLE_SETOPT_OPTION_SYNTAX',      // NOT USED
+        50  =>  'CURLE_OBSOLETE50',                // NOT USED
+        51  =>  'CURLE_OBSOLETE51',
         52  =>  'CURLE_GOT_NOTHING',
         53  =>  'CURLE_SSL_ENGINE_NOTFOUND',
         54  =>  'CURLE_SSL_ENGINE_SETFAILED',
         55  =>  'CURLE_SEND_ERROR',
-        56  =>  'CURLE_RECV_ERROR',
+        56  =>  'CURLE_RECV_ERROR',                // NOT USED
+        57  =>  'CURLE_OBSOLETE57',
         58  =>  'CURLE_SSL_CERTPROBLEM',
         59  =>  'CURLE_SSL_CIPHER',
-        60  =>  'CURLE_SSL_CACERT',
-        61  =>  'CURLE_BAD_CONTENT_ENCODING',
-        62  =>  'CURLE_LDAP_INVALID_URL',
+        60  =>  'CURLE_PEER_FAILED_VERIFICATION',
+        61  =>  'CURLE_BAD_CONTENT_ENCODING',      // NOT USED SINCE curl 7.82.0
+        62  =>  'CURLE_OBSOLETE62',
         63  =>  'CURLE_FILESIZE_EXCEEDED',
         64  =>  'CURLE_USE_SSL_FAILED',
         65  =>  'CURLE_SEND_FAIL_REWIND',
@@ -177,8 +190,8 @@ class Zebra_cURL {
         72  =>  'CURLE_TFTP_UNKNOWNID',
         73  =>  'CURLE_REMOTE_FILE_EXISTS',
         74  =>  'CURLE_TFTP_NOSUCHUSER',
-        75  =>  'CURLE_CONV_FAILED',
-        76  =>  'CURLE_CONV_REQD',
+        75  =>  'CURLE_CONV_FAILED',               // NOT USED SINCE curl 7.82.0
+        76  =>  'CURLE_OBSOLETE76',
         77  =>  'CURLE_SSL_CACERT_BADFILE',
         78  =>  'CURLE_REMOTE_FILE_NOT_FOUND',
         79  =>  'CURLE_SSH',
@@ -191,6 +204,16 @@ class Zebra_cURL {
         86  =>  'CURLE_RTSP_SESSION_ERROR',
         87  =>  'CURLE_FTP_BAD_FILE_LIST',
         88  =>  'CURLE_CHUNK_FAILED',
+        89  =>  'CURLE_NO_CONNECTION_AVAILABLE',
+        90  =>  'CURLE_SSL_PINNEDPUBKEYNOTMATCH',
+        91  =>  'CURLE_SSL_INVALIDCERTSTATUS',
+        92  =>  'CURLE_HTTP2_STREAM',
+        93  =>  'CURLE_RECURSIVE_API_CALL',
+        94  =>  'CURLE_AUTH_ERROR',
+        95  =>  'CURLE_HTTP3',
+        96  =>  'CURLE_QUIC_CONNECT_ERROR',
+        97  =>  'CURLE_PROXY',
+        98  =>  'CURLE_SSL_CLIENTCERT',
     );
 
     /**
