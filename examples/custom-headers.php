c11e2a8b2c6fbe7f7113b5e149d5c1a638c0f05a
stefangabos <contact@stefangabos.ro>
2022-06-17 11:13:25 +0300

    Fixed example

--- examples/custom-headers.php
+++ examples/custom-headers.php
@@ -16,6 +16,11 @@ require '../Zebra_cURL.php';
 // instantiate the Zebra_cURL class
 $curl = new Zebra_cURL();
 
+// since we are communicating over HTTPS, we load the CA bundle from the examples folder,
+// so we don't get CURLE_SSL_CACERT response from cURL
+// you can always update this bundle from https://curl.se/docs/caextract.html
+$curl->ssl(true, 2, __DIR__ . '/cacert.pem');
+
 // set custom HTTP headers
 $curl->option(CURLOPT_HTTPHEADER, [
     'accept: application/json',
