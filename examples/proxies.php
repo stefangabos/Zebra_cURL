2208e0b233ba798f502121c18b139b0acc82417b
Stefan Gabos <contact@stefangabos.ro>
2023-11-25 19:48:05 +0200

    Updated example

--- examples/proxies.php
+++ examples/proxies.php
@@ -4,9 +4,22 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-// instantiate Zebra_cURL
+// make sure cache folder exists and is writable
+if (!is_dir('cache') || !is_writable('cache')) trigger_error('the "cache" folder must be present and be writable in the "examples" folder', E_USER_ERROR);
+
+// make sure CA bundle exists
+elseif (!file_exists('cacert.pem')) trigger_error('"cacert.pem" file was not found', E_USER_ERROR);
+
+// include the library
 require '../Zebra_cURL.php';
+
+// instantiate the Zebra_cURL class
 $curl = new Zebra_cURL();
+
+// since we are communicating over HTTPS, we load the CA bundle from the examples folder,
+// so we don't get CURLE_SSL_CACERT response from cURL
+// you can always update this bundle from https://curl.se/docs/caextract.html
+$curl->ssl(true, 2, __DIR__ . '/cacert.pem');
 
 // one or more URLs
 $queue = array(
